57a02a162e3d4e9e8a91f421b35990e9e24a1365
nzaouesgi <hakhenaton@protonmail.com>
2020-06-17 11:33:15 +0200

    changed filename

--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,28 @@ const app = express()
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(YAML.load(path.resolve(__dirname, '../swagger.yaml'))))
 app.use(cors())
 
+// job API
 app.use('/jobs', jobRouter())
 
+// 404 handler
 app.all('*', (_req, res, _next) => {
     res.status(404).json({
         errors: [ 'Not found' ]
     })
 })
 
-// Error handler
+// error handler
 app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
     res.status(500).json({
         errors: [ error.message ]
     })
 });
 
-(async () => {
-    
-    await mongoose.connect(`mongodb://${process.env['COMMANDS_MONGO_HOST']}:${process.env['COMMANDS_MONGO_PORT']}/${process.env['COMMANDS_MONGO_DATABASE']}`, {
+const start = async () => {
+
+    const mongoConnectionString = `mongodb://${process.env['COMMANDS_MONGO_HOST']}:${process.env['COMMANDS_MONGO_PORT']}/${process.env['COMMANDS_MONGO_DATABASE']}`
+
+    await mongoose.connect(mongoConnectionString, {
         useNewUrlParser: true,
         bufferCommands: false,
         useUnifiedTopology: true,
@@ -45,16 +49,21 @@ app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
         autoCreate: true
     })
 
-    mongoose.connection.on('error', console.error)
-    mongoose.connection.on('disconnected', console.error)
-    mongoose.connection.on('reconnected', console.info)
+    mongoose.connection.on('error', (err) => {
+        console.error(`MongoDB connection error: ${err.message}`)
+    })
+    mongoose.connection.on('disconnected', () => { 
+        console.error(`Disconnected from ${mongoConnectionString}`) 
+    })
+    mongoose.connection.on('reconnected', () => {
+        console.info(`Reconnected to ${mongoConnectionString}`)
+    })
 
     const port = process.env.PORT || 80
 
     app.listen(port, () => {
         console.log(`API is listening on port ${port}`)
     })
+}
 
-})()
-
-.catch(console.error)
+start().catch(console.error)
