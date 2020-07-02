852d507ab80260c58071e1b47e6995fc0c497ad8
nzaouesgi <hakhenaton@protonmail.com>
2020-07-02 22:57:18 +0200

    added execute job with amqp to docker-runner

--- src/amqp.ts
+++ src/amqp.ts
@@ -16,10 +16,10 @@ export class AmqpClient {
         const host = process.env['COMMANDS_RABBITMQ_HOST']
         const port = process.env['COMMANDS_RABBITMQ_PORT']
 
-        this.connection = amqp.connect([`amqp://${user}:???@${host}:${port}`])
+        this.connection = amqp.connect([`amqp://${user}:${password}@${host}:${port}`])
 
         this.connection.on('connect', ({url}) => {
-            console.log(`successfully connected to ${url}`)
+            console.log(`successfully connected to amqp broker.`)
         })
 
         this.connection.on('disconnect', ({err}) => {
